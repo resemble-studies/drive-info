9b33a4e25c0be33c0ee9ef85683905ecb80766bb
yvan-burrie <yvan.burrie@hotmail.com>
2020-11-21 22:44:06 +1000

    Update drive-info.cpp

--- src/ensemble/drive-info.cpp
+++ src/ensemble/drive-info.cpp
@@ -1,8 +1,10 @@
 /**
  * License: MIT. See LICENSE in root directory.
- * \date 2020/06/27
+ * \date 2020/11/21
  */
 
+#include <io.h>
+#include <cstdlib>
 #include <cstdio>
 #include <cstring>
 #include <direct.h>
@@ -11,7 +13,20 @@
 
 namespace Ensemble {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+int compareFilenameStrings(void* e1, void* e2)
+{
+    return strcmp((const char*)e1, (const char*)e2);
+}
+
+int driveAsLetter(int d)
+{
+    return d + 64;
+}
+
+int driveAsInt(char d)
+{
+    return d - 64;
+}
 
 DriveInformation::DriveInformation()
     :
@@ -22,63 +37,50 @@ DriveInformation::DriveInformation()
     initialDirectoryValue = currentDirectoryValue;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 int DriveInformation::initialDrive() const
 {
     return initialDriveValue;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 char DriveInformation::initialDriveAsLetter() const
 {
-    return initialDriveValue + 64;
+    return driveAsLetter(initialDriveValue);
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const char* DriveInformation::initialDirectory()
 {
     return initialDirectoryValue.c_str();
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 int DriveInformation::currentDrive() const
 {
     return currentDriveValue;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 char DriveInformation::currentDriveAsLetter() const
 {
-    return currentDriveValue + 64;
+    return driveAsLetter(currentDriveValue);
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const char* DriveInformation::currentDirectory()
 {
     return currentDirectoryValue.c_str();
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+int DriveInformation::maxNumberDrives() const
+{
+    return sizeof(driveValues) / sizeof(*driveValues);
+}
 
 int DriveInformation::numberDrives()
 {
     return driveValues[0];
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 int DriveInformation::validDrive(int v)
 {
     return driveValues[v];
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void DriveInformation::checkDrives()
 {
@@ -98,8 +100,6 @@ void DriveInformation::checkDrives()
     ::_chdrive(currentDriveValue);
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 int DriveInformation::numberOfFilesWithFilename(const char* n)
 {
     ::_finddata_t fileInfo {};
@@ -117,8 +117,6 @@ int DriveInformation::numberOfFilesWithFilename(const char* n)
 
     return 0;
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int DriveInformation::numberOfFilesWithAttribute(int a)
 {
@@ -141,12 +139,10 @@ int DriveInformation::numberOfFilesWithAttribute(int a)
     return 0;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 int DriveInformation::numberOfFilesWithFilenameOrAttribute(const char* n, int a)
 {
     ::_finddata_t fileInfo {};
-    auto found = ::_findfirst("*.*", &fileInfo);
+    int found = ::_findfirst("*.*", &fileInfo);
     if (found != -1)
     {
         int num_files = 1;
@@ -164,28 +160,20 @@ int DriveInformation::numberOfFilesWithFilenameOrAttribute(const char* n, int a)
     return 0;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 bool DriveInformation::resetCurrentDriveAndDirectory()
 {
-    return resetCurrentDrive() ? resetCurrentDirectory() : false;
+    return resetCurrentDrive() && resetCurrentDirectory();
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 bool DriveInformation::resetCurrentDrive()
 {
     return changeDrive(initialDriveValue);
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 bool DriveInformation::resetCurrentDirectory()
 {
     return changeDirectory(initialDirectoryValue.c_str());
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 bool DriveInformation::changeDrive(int d)
 {
@@ -199,8 +187,6 @@ bool DriveInformation::changeDrive(int d)
     return false;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 bool DriveInformation::changeDirectory(const char* d)
 {
     if (::_chdir(d) == 0)
@@ -213,14 +199,12 @@ bool DriveInformation::changeDirectory(const char* d)
     return false;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 void DriveInformation::updateDirectory()
 {
-    char fullPath[FILENAME_MAX] {};
+    char fullPath[_MAX_PATH] {};
     ::_getcwd(fullPath, sizeof(fullPath));
 
-    char cDrive[3], filename[256], extension[256];
+    char cDrive[_MAX_DRIVE], filename[_MAX_FNAME], extension[_MAX_EXT];
     ::_wsplitpath(
         (wchar_t*)fullPath,
         (wchar_t*)cDrive,
@@ -228,7 +212,7 @@ void DriveInformation::updateDirectory()
         (wchar_t*)filename,
         (wchar_t*)extension);
 
-    char tempString[0x100];
+    char tempString[_MAX_DIR];
     ::snprintf(
         tempString,
         sizeof(tempString),
@@ -236,9 +220,8 @@ void DriveInformation::updateDirectory()
         currentDirectoryValue.c_str(),
         filename,
         extension);
+
     currentDirectoryValue = tempString;
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
 } // namespace Ensemble
