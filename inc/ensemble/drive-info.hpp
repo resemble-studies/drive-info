39c0e758b5ce0210e65eac091022e50ae483466a
yvan-burrie <yvan.burrie@hotmail.com>
2020-11-21 22:44:00 +1000

    Update drive-info.hpp

--- inc/ensemble/drive-info.hpp
+++ inc/ensemble/drive-info.hpp
@@ -1,6 +1,6 @@
 /**
  * License: MIT. See LICENSE in root directory.
- * \date 2020/06/27
+ * \date 2020/11/21
  */
 
 #pragma once
@@ -9,19 +9,19 @@
 
 namespace Ensemble {
 
-/**
- *
- */
+inline int compareFilenameStrings(void* e1, void* e2);
+
+inline int driveAsLetter(int d);
+inline int driveAsInt(char d);
+
 class DriveInformation {
 
 protected:
 
     int initialDriveValue = 0;
-
     std::string initialDirectoryValue {};
 
     int currentDriveValue = 0;
-
     std::string currentDirectoryValue {};
 
     int driveValues[1 + 26] {};
@@ -33,42 +33,30 @@ public:
 public:
 
     int initialDrive() const;
-
     char initialDriveAsLetter() const;
-
     const char* initialDirectory();
 
     int currentDrive() const;
-
     char currentDriveAsLetter() const;
-
     const char* currentDirectory();
 
-    int maxNumberDrives() const
-    { return sizeof(driveValues) / sizeof(*driveValues); }
-
+    int maxNumberDrives() const;
     int numberDrives();
-
     int validDrive(int);
 
     void checkDrives();
 
     static int numberOfFilesWithFilename(const char*);
-
     static int numberOfFilesWithAttribute(int);
-
     static int numberOfFilesWithFilenameOrAttribute(const char*, int);
-
-    bool resetCurrentDriveAndDirectory();
 
 private:
 
+    bool resetCurrentDriveAndDirectory();
     bool resetCurrentDrive();
-
     bool resetCurrentDirectory();
 
     bool changeDrive(int);
-
     bool changeDirectory(const char*);
 
     void updateDirectory();
